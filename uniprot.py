26e647b41e910171af516d1a064479029e9c7dd6
Estelle Ronayne <estelleronayne@pcd-1xvp3y2-lt.ucsfmedicalcenter.org>
2021-10-13 10:40:20 -0700

    modified:   uniprot.py

--- uniprot.py
+++ uniprot.py
@@ -3,8 +3,15 @@
 import pandas as pd
 import numpy as np
 
+#read tsv file of cytosolic proteins
 cytosol = pd.read_csv("subcell_location_cytosol.tsv", sep='\t')
+
+#extract Uniprot IDs
 uniprot = cytosol["Uniprot"]
+
+#delete rows without Uniprot ID
 df = uniprot.dropna()
+
+#reindex
 df1 = df.reset_index(drop=True)
 print(df1)
